fe47fe81145e7b0601ea589a758d9e57fc7b0c5a
RaviKishan24 <Ravikishankumar71@gmail.com>
2026-06-06 20:29:42 +0530

    changes added

--- Client/api/apiNews.js
+++ Client/api/apiNews.js
@@ -1,15 +1,21 @@
 export default async function handler(req, res) {
     try {
+        let { url } = req.query;
 
-        const { url } = req.query;
         url = decodeURIComponent(url);
 
-        const response = await fetch(`https://newsapi.org/v2${url}&apiKey=${process.env.NEWS_API_KEY}` );
+        const baseURL = "https://newsapi.org/v2";
+
+        const response = await fetch(
+            `${baseURL}${url}${url.includes("?") ? "&" : "?"}apiKey=${process.env.NEWS_API_KEY}`
+        );
 
         const data = await response.json();
 
         res.status(200).json(data);
+
     } catch (error) {
+        console.log(error);
         res.status(500).json({ message: "Server Error" });
     }
 }
